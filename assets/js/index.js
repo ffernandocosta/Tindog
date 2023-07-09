29470231ad289ce968caecacfac6389529b4d2eb
Fernando Costa <ferndcosta353@gmail.com>
2023-07-09 19:23:34 -0300

    feat: add swipe dog functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,3 +14,61 @@ function getNextDog() {
     // Returning the 'nextDog' if it exists, otherwise returning an empty object
     return nextDog ? nextDog : {};
 }
+
+// Function to handle swiping a dog
+function swipeDog(hasBeenLiked) {
+    // Checking if the dog has already been swiped
+    if (!dog.hasBeenSwiped) {
+        // Marking the dog as swiped and setting its liked status
+        dog.hasBeenSwiped = true;
+        dog.hasBeenLiked = hasBeenLiked;
+
+        // Toggling the visibility of the 'like-badge' or 'nope-badge' based on the liked status
+        dog.hasBeenLiked
+            ? document.getElementById('like-badge').toggleAttribute('data-visible')
+            : document.getElementById('nope-badge').toggleAttribute('data-visible');
+
+        // Checking if there are more dogs in the array
+        if (dogsArray.length > 0) {
+            // Delaying the rendering of the next dog by 1500 milliseconds
+            setTimeout(() => {
+                // Getting the next dog and updating the 'dog' variable
+                dog = getNextDog();
+                // Rendering the updated dog
+                render();
+            }, 1500);
+        } else {
+            // Displaying a message when there are no more dogs
+            endOfDogsMessage();
+        }
+    }
+}
+
+// Function to display a message when there are no more dogs
+function endOfDogsMessage() {
+    // Delaying the alert message by 1500 milliseconds
+    setTimeout(() => {
+        alert('No more dogs available in your area');
+    }, 1500);
+}
+
+// Adding an event listener to the 'like-button' element
+document.getElementById('like-button').addEventListener('click', () => {
+    // Calling 'swipeDog' with the 'hasBeenLiked' parameter set to true
+    swipeDog(true);
+});
+
+// Adding an event listener to the 'reject-button' element
+document.getElementById('reject-button').addEventListener('click', () => {
+    // Calling 'swipeDog' with the 'hasBeenLiked' parameter set to false
+    swipeDog(false);
+});
+
+// Function to render the currently displayed dog
+function render() {
+    // Updating the HTML content of the 'dogs' element with the dog's HTML representation
+    document.getElementById('dogs').innerHTML = dog.getDogHtml();
+}
+
+// Initial rendering of the first dog
+render();
