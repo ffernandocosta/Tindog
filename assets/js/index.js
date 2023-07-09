d937f032d12869e08e3c00f433a397c10809950d
Fernando Costa <ferndcosta353@gmail.com>
2023-07-09 17:58:09 -0300

    feat: add 'getNextDog' function
    add 'getNextDog' function to return the next dog in 'dogsArray' and store in a variable 'nextDog'

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,3 +3,14 @@ import Dog from "./Dog.js";
 
 // Creating a new array of dogs by creating new instances of dogs with the 'Dog' class
 const dogsArray = dogsData.map((dog) => new Dog(dog));
+
+// Variable to hold the currently displayed dog
+let dog = getNextDog();
+
+// Function to get the next dog from the array
+function getNextDog() {
+    // Removing the first dog from the array and storing it in 'nextDog'
+    const nextDog = dogsArray.shift();
+    // Returning the 'nextDog' if it exists, otherwise returning an empty object
+    return nextDog ? nextDog : {};
+}
